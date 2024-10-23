03de9cfc2d2595de7dc1720a0842f72794d2a97a
fardinariful <fardinewucse2020@gmail.com>
2024-10-23 14:43:39 +0600

    some info added

--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,10 +1,12 @@
 import './Country.css'
 const Country = ({country}) => {
-   const {name,flags} = country;
+   const {name,flags,area,population} = country;
     return (
         <div className='country'>
             <h3>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
+            <p>Population:{population}</p>
+            <h3>Area:{area}</h3>
         </div>
     );
 };
