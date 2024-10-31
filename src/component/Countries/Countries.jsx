b18db5b74abb9e45235a33548e2a223a93f7c536
fardinariful <fardinewucse2020@gmail.com>
2024-10-31 08:51:28 +0600

    something added

--- src/component/Countries/Countries.jsx
+++ src/component/Countries/Countries.jsx
@@ -5,21 +5,30 @@ import './Countries.css'
 
 const Countries = () => {
     const [countries,setcountries] = useState([]);
-
+      
+    const [visitedCountries,setvisitedCountries]=useState([]);
     useEffect(()=>
         {
             fetch('https://restcountries.com/v3.1/all')
             .then(res => res.json())
             .then(data=>setcountries(data))
         },[])
+
+        const handleclick2=country=>{
+            console.log("Add these on your visited Country");
+            console.log(country);
+        }
     return (
         <div >
             <div>
             <h3>Countries:{countries.length}</h3>
             </div>
+            <div>
+                <h5>Visited Countries</h5>
+            </div>
             <div className="countries_container">
             {
-                countries.map(country=><Country key={country.cca3} country={country}></Country>)
+                countries.map(country=><Country key={country.cca3} handleclick2={handleclick2} country={country}></Country>)
             }
             </div>
             
--- src/component/Country/Country.jsx
+++ src/component/Country/Country.jsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import './Country.css'
-const Country = ({country}) => {
+const Country = ({country,handleclick2}) => {
    const {name,flags,area,population,cca3} = country;
-
+    console.log(handleclick2);
    const [Visited,setVisited] = useState(false);
    const handleclick1=()=>{
     setVisited(!Visited);
    }
     return (
-        <div className='country'>
-            <h3>Name: {name.common}</h3>
+        <div className={`country ${Visited ? 'visited' : 'not_visited'}`}>
+            <h3 style={{color: Visited ? 'purple' : 'red'}}>Name: {name.common}</h3>
             <img src={flags.png} alt="" />
             <p>Population:{population}</p>
             <h3>Area:{area}</h3>
